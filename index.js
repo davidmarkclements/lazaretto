cfb4aa0c572df82caee5985fb72e4f890a0e4f74
David Mark Clements <david.mark.clements@gmail.com>
2020-10-09 05:36:43 +0200

    mocking capabilities (cjs)

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const { pathToFileURL } = require('url')
 const { dirname, join } = require('path')
 const { promisify } = require('util')
 const { readFile } = require('fs').promises
-const { Worker}  = require('worker_threads')
+const { Worker } = require('worker_threads')
+const mockery = require('./lib/mockery')
 const cp = require('child_process')
 
-async function lazaretto ({ esm = false, entry, scope = [], prefix = '', suffix = '', ...opts } = {}) {
+async function lazaretto ({ esm = false, entry, scope = [], context = {}, mock, prefix = '' } = {}) {
   if (Array.isArray(scope) === false) scope = [scope]
   const scopeParams = scope.length ? scope.map((ref) => {
     if (typeof ref === 'string') return `'${ref}'`
@@ -19,14 +20,16 @@ async function lazaretto ({ esm = false, entry, scope = [], prefix = '', suffix 
     if (ref !== null && typeof ref === 'object') {
       ref = Object.entries(ref).pop()
       ref = ref && ref[1]
-      if (ref === 'import.meta') return `global[Symbol.for('kLazarettoImportMeta')]`
+      if (ref === 'import.meta') return 'global[Symbol.for(\'kLazarettoImportMeta\')]'
       return ref
     }
     return ref
   }).filter(Boolean)
+
   const relativeDir = dirname(entry)
   const entryUrl = pathToFileURL(entry).href
   const include = esm ? 'await import' : 'require'
+  const mocking = mockery(mock, { esm, entry })
   const shims = (esm ? `
     import.meta.url = '${entryUrl}';
     global[Symbol.for('kLazarettoImportMeta')] = import.meta
@@ -37,42 +40,71 @@ async function lazaretto ({ esm = false, entry, scope = [], prefix = '', suffix 
     require.main = module
     module.require = require
     module.paths = require.resolve.paths(__filename)
+    ${mocking ? mocking.scopeMocks() : ''}
   `).split('\n').map((s) => s.trim() + ';').join('')
   const comms = `
     ${shims}
-    const { parentPort } = ${include}('worker_threads')
-    parentPort.on('message', ([cmd, args]) => {
-      if (cmd === 'init') parentPort.postMessage('init')
-      if (cmd === 'expr') {
-        const expr = args.shift()
-        const fn = new Function(${scopeParams}'return ' + expr)
-        parentPort.postMessage(fn(${scopeArgs}))
-      }
-    })
+    {
+      const { parentPort } = ${include}('worker_threads')
+      parentPort.on${esm ? '' : 'ce'}('message', ([cmd, args]) => {
+        if (cmd === 'init') {
+          ${esm ? '' : 'global[Symbol.for(\'kLazarettoInit\')]()'}
+          parentPort.postMessage(['init'])
+        }
+        ${esm ? `if (cmd === 'expr') {
+          const expr = args.shift()
+          const fn = new Function(${scopeParams}'return ' + expr)
+          parentPort.postMessage([cmd, fn(${scopeArgs})])
+        }` : ''}
+      })
+    }
   `.split('\n').map((s) => s.trim() + ';').join('')
   const contents = await readFile(entry, 'utf8')
-  const code = `${prefix}${comms}${contents}${suffix}`
+  const wrapped = esm ? contents : `global[Symbol.for('kLazarettoInit')] = () => {
+    {
+      const { parentPort } = require('worker_threads')
+      parentPort.on('message', ([cmd, args]) => {
+        if (cmd === 'expr') {
+          const expr = args.shift()
+          const fn = new Function(${scopeParams}'return ' + expr)
+          parentPort.postMessage([cmd, fn(${scopeArgs})])
+        }
+      })
+    }`.split('\n').map((s) => s.trim() + ';').join('') + contents + '\n}'
+  const code = `${prefix}${comms}${wrapped}`
+  const exec = esm
+    ? new URL(`data:text/javascript;base64,${Buffer.from(code).toString('base64')}`)
+    : code
 
-  const exec = esm ?
-    new URL(`data:text/javascript;base64,${Buffer.from(code).toString('base64')}`) :
-    code
-  
   if (esm) {
     process.env.LAZARETTO_LOADER_DATA_URL = exec.href
     process.env.LAZARETTO_LOADER_RELATIVE_DIR = relativeDir
   }
 
+  if (mocking) process.env.LAZARETTO_OVERRIDES = JSON.stringify(mocking)
+
   const worker = new Worker(exec, {
-    ...opts,
     eval: !esm,
-    execArgv: esm ? [...process.execArgv, ...(opts.execArgv || []), '--no-warnings', `--experimental-loader=${join(__dirname, 'loader.mjs')}`] : process.execArgv
-   })
+    workerData: { context },
+    execArgv: esm
+      ? [...process.execArgv, '--no-warnings', `--experimental-loader=${join(__dirname, 'lib', 'loader.mjs')}`]
+      : [...process.execArgv, '--no-warnings', '-r', `${join(__dirname, 'lib', 'preload.js')}`]
+  })
+
+  worker.on('message', ([cmd, o]) => {
+    if (cmd !== 'context') return
+    Object.assign(context, o)
+  })
+
   await hook('init')
 
   const sandbox = async (code, ...args) => {
-    const [ result ] = await hook('expr', [code, ...args])
+    const [result] = await hook('expr', [code, ...args])
     return result
   }
+
+  sandbox.context = context
+
   sandbox.fin = () => {
     if (esm) {
       delete process.env.LAZARETTO_LOADER_DATA_URL
@@ -85,7 +117,8 @@ async function lazaretto ({ esm = false, entry, scope = [], prefix = '', suffix 
   function hook (cmd, args = []) {
     return promisify((worker, cb) => {
       let done = false
-      const msg = (...args) => {
+      const msg = ([cmdIn, ...args]) => {
+        if (cmdIn !== cmd) return
         if (done === false) {
           cb(null, args)
           return
@@ -96,28 +129,28 @@ async function lazaretto ({ esm = false, entry, scope = [], prefix = '', suffix 
       const error = (err) => {
         if (done === false) {
           const stack = err.stack.split('\n')
-          let frame = esm ?
-            stack.find((frame) => /data:text\/javascript;base64,/.test(frame)) :
-            stack[0]
-          
-          let restack = esm ? 
-            err.stack.replace(RegExp(exec.toString().replace(/([\+|\/])/g, '\\$1'), 'gm'), entry) : 
-            err.stack.replace(/\[worker eval\]:/gm, entry + ':')
+          let frame = esm
+            ? stack.find((frame) => /data:text\/javascript;base64,/.test(frame))
+            : stack[0]
+
+          let restack = esm
+            ? err.stack.replace(RegExp(exec.toString().replace(/([+|/])/g, '\\$1'), 'gm'), entry)
+            : err.stack.replace(/\[worker eval\]:/gm, entry + ':')
 
           if (esm && !frame && err instanceof SyntaxError) {
-            const { status, stderr } = cp.spawnSync(process.execPath, ['-c', entry], {encoding: 'utf8'})
+            const { status, stderr } = cp.spawnSync(process.execPath, ['-c', entry], { encoding: 'utf8' })
             if (status) {
               frame = ':' + stderr.split('\n')[0]
               restack = `${stack[0]}\n    at ${entry}:${frame.split(':')[2]}`
             }
           }
-  
+
           if (frame) {
             const line = esm ? +frame.split(':')[2] : +frame.split(':')[1]
             const msgRx = RegExp(err.message)
             const banner = stack.find((line) => msgRx.test(line))
             err = Object.create(err)
-            
+
             err.stack = restack
             err.line = line
             const diagnosis = contents.split('\n')
@@ -129,18 +162,18 @@ async function lazaretto ({ esm = false, entry, scope = [], prefix = '', suffix 
             err.diagnosis = diagnosis.slice(from, to).map((line, n) => `${n + from}: ${line}`).join('\n')
             err.esm = esm
           }
-  
+
           cb(err)
           return
         }
         done = true
         worker.removeListener('message', msg)
       }
-      worker.once('message', msg)
+
+      worker.on('message', msg)
       worker.once('error', error)
       worker.postMessage([cmd, args])
     })(worker)
   }
 }
-
 module.exports = lazaretto
--- readme.md
+++ readme.md
@@ -30,7 +30,7 @@ const lazaretto = require('lazaretto')
 import lazaretto from 'lazaretto'
 ```
 
-### `await lazaretto({ esm = false, entry, scope, prefix, suffix, ...opts }) => sandbox <(expression: String) => result)>`
+### `await lazaretto({ esm = false, entry, scope, mock, context, prefix }) => sandbox <(expression: String) => result)>`
 
 #### Options
 
@@ -50,19 +50,53 @@ For instance, let's say we want to run code in a sandbox that has a function nam
 We would set the `scope` option to `['fn']`. 
 
 
+##### `mock` - Object
+
+The `mock` object can be used to override natives and libraries. The mocking of the following is supported
+
+* builtin modules (`fs`, `path`, `child_process`...)
+* globals (`process`, `Buffer`, `setTimeout`...)
+* module-scoped variables (`__dirname`, `__filename`, `require`...) *CJS modules only*
+* project-local libraries (`./path/to/file.js`, `/absolute/path/to/file.js`), resolution is relative to the `entry` path.
+* project dependencies (as specified in `package.json`)
+
+To mock supply the mocking target name as a key of the object and set it to a handler function: 
+
+```js
+  const mock = {
+    async fs (fs, { context, include }) { return {mock: 'fs'} }
+    ['./path/to/local-lib.js']: async (mod, { context, include }) => {
+      return {another: 'mock'}
+    },
+    __dirname(__dirname, { context, require }) {
+      return '/override/dirname'
+    }
+  }
+  const sandbox = await lazaretto({ esm, entry, mock })
+```
+
+All handler functions except module-scoped variable handler functions **may** return a promise (e.g. be an `async function`). 
+
+Module-scoped variable handler functions (e.g. `__dirname` etc.), **must** be synchronous functions. 
+
+The handler function has the signature `(original, api) => {}` where `original` is the original value of the 
+mock-target and `api` contains utilities for cross-module-system and cross-isolate interactions.
+
+For all handler functions, `api.context` is an object which can be used to store state within the sandbox, 
+this state will then be available in the main thread at [`sandbox.context`](#sandbox.context-object). 
+
+For all handler function except module-scoped, there is an `api.include` function. This works in a similar
+way to [Dynamic Import](https://wiki.developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import#Dynamic_Imports), 
+except it smooths over the `default` ugliness.
+
+
+##### `context` - Object, default: {}
+
+Sets the initial context that is then passed to mock handler functions. See [`sandbox.context`](#sandbox.context-object)
+
 ##### `prefix` - String, default: ''
 
 Inject code at the top of `entry` contents prior to execution.
-
-
-##### `suffix` - String, default: ''
-
-Inject code at the bottom of `entry` contents prior to execution.
-
-
-##### `...opts`
-
-Any additional options will be passed into the [`Worker`](https://nodejs.org/dist/latest-v14.x/docs/api/worker_threads.html#worker_threads_new_worker_filename_options) constructor
 
 
 #### `sandbox` <(expression: String) => result)>
@@ -85,6 +119,15 @@ assert.strict.equal(sandbox(`fn()`), true)
 ```
 
 Data return from evaluating an expression in the sandbox is cloned from the isolate thread according to the [HTML structured clone algorithm](https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm) which means you can't return functions, and a Node `Buffer` will be cloned as a `Uint8Array` - see https://nodejs.org/api/worker_threads.html#worker_threads_considerations_when_transferring_typedarrays_and_buffers.
+
+##### `sandbox.context` - Object
+
+The `sandbox.context` object is synchronised with any changes made to the `api.context` object in any of the mocks.
+Any state stored on context is passed between the main thread and the worker thread (and vice-versa), this means the
+[HTML structured clone algorithm](https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm) 
+is used to synchronize the main and worker thread context objects. Therefore functions cannot be transferred and there
+are caveats around how to handle buffers.
+
 
 ## Todo
 
